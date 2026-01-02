c1be33a2d42084f45693b0fcb0cb78812f3ad5d7
Nox Dev <noxdev85@gmail.com>
2026-01-02 14:52:29 +0100

    Base Template V4

--- resources/[ESX]/esx_identity/web/src/components/Identity.vue
+++ resources/[ESX]/esx_identity/web/src/components/Identity.vue
@@ -39,49 +39,49 @@ const schema = yup.object({
             <p class="dialog__body-hint">Start by creating your identity</p>
             <Form class="dialog__body-form" id="register" action="#" novalidate @submit="onSubmit" :validation-schema="schema">
                 <div class="dialog__form-group">
-                    <label for="firstname">Prénom</label>
+                    <label for="firstname">Firstname</label>
                     <div class="dialog__form-validation">
-                        <Field id="firstname" type="text" name="firstname" placeholder="Prénom" validateOnInput />
+                        <Field id="firstname" type="text" name="firstname" placeholder="Firstname" validateOnInput />
                     </div>
                     <ErrorMessage name="firstname" class="dialog__form-message dialog__form-message--error" />
                 </div>
                 <div class="dialog__form-group">
-                    <label for="lastname">Nom</label>
+                    <label for="lastname">Lastname</label>
                     <div class="dialog__form-validation">
-                        <Field id="lastname" type="text" name="lastname" placeholder="Nom" validateOnInput />
+                        <Field id="lastname" type="text" name="lastname" placeholder="Lastname" validateOnInput />
                     </div>
                     <ErrorMessage name="lastname" class="dialog__form-message dialog__form-message--error" />
                 </div>
                 <div class="dialog__form-group">
-                    <label for="dob">Date de naissance</label>
+                    <label for="dob">Date of Birth</label>
                     <Field id="dob" type="date" name="dob" placeholder="dd/mm/yyyy" validateOnInput />
                     <ErrorMessage name="dob" class="dialog__form-message dialog__form-message--error" />
                 </div>
                 <div class="dialog__form-group">
-                    <label for="gender">Genre</label>
+                    <label for="gender">Gender</label>
                     <div class="dialog__form-group dialog__form-group--radio">
                         <div class="dialog__form-radio">
                             <Field type="radio" id="male" value="m" name="gender" validateOnInput />
                             <label for="male">
-                                <i class="fas fa-mars"></i>Homme
+                                <i class="fas fa-mars"></i>Male
                             </label>
                         </div>
                         <div class="dialog__form-radio">
                             <Field type="radio" id="female" value="f" name="gender" validateOnInput />
                             <label for="female">
-                                <i class="fas fa-venus"></i>Femme
+                                <i class="fas fa-venus"></i>Female
                             </label>
                         </div>
                     </div>
                     <ErrorMessage name="gender" class="dialog__form-message dialog__form-message--error" />
                 </div>
                 <div class="dialog__form-group">
-                    <label for="height">Traille</label>
+                    <label for="height">Height</label>
                     <Field id="height" type="text" name="height" placeholder="175" validateOnInput/>
                     <ErrorMessage name="height" class="dialog__form-message dialog__form-message--error" />
                 </div>
                 <button class="dialog__form-submit" id="submit" type="submit">
-                    <i class="fas fa-user-plus"></i>Valider
+                    <i class="fas fa-user-plus"></i>Create
                 </button>
             </Form>
         </div>
